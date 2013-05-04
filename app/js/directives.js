7c4fe733a756e0cc60acf7fe1a774adf0cfad12f
Rakshit Majithiya <rakshit@motioneleven.com>
2013-05-04 11:09:54 -0400

    Remove unused directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,4 @@
 /* Directives */
 
 
-angular.module('pocketYoutube.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('pocketYoutube.directives', []);
