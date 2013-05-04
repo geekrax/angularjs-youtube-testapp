b85fa8f31b1e29db8f14ba001fcaa7ff005b33ab
Rakshit Majithiya <rakshit@motioneleven.com>
2013-05-03 23:08:38 -0400

    + Static json containing list of videos (just for development purpose) + provided 'videos' object by fetching json + Roughly show list of videos and important metadata

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,19 @@
 
 angular.module('pocketYoutube.controllers', [])
 
-.controller('HomeCtrl', ['$scope', function($scope) {
-  $scope.message = 'The 5by Challenge!';
+.controller('HomeCtrl', ['$scope','$http', function($scope,$http) {
+  
+  //Set welcome message (just a simple for test)
+  $scope.welcome_message = 'Welcome to Pocket Youtube';
+
+  // Initially, use static data
+  // Reference : http://docs.angularjs.org/tutorial/step_05
+  $http.get('videos/list.json').success(function(data) {
+  	$scope.videos = [];
+    data.forEach(function(video){
+    	$scope.videos.push(video.data.items[0]);
+    });
+  });
+ 
+
 }]);
